88ef51b52806f4d4f87fdf7b0f81fd474cd9708d
Brian Stewart <brianstewarteditor@gmail.com>
2021-08-24 22:22:04 +1000

    alter font size on resize,handle resize edge cases

--- source/core/src/main/com/deco2800/game/components/mainmenu/MainMenuDisplay.java
+++ source/core/src/main/com/deco2800/game/components/mainmenu/MainMenuDisplay.java
@@ -3,15 +3,16 @@ package com.deco2800.game.components.mainmenu;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
 import com.deco2800.game.services.ServiceLocator;
 import com.deco2800.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
 
 /**
  * A ui component for displaying the Main menu.
@@ -21,16 +22,22 @@ public class MainMenuDisplay extends UIComponent {
   private static final float Z_INDEX = 2f;
   private Table table;
   private Image background;
+  private ArrayList<TextButton> menuButtons;
   private static final float BACKGROUND_IMAGE_ASPECT = 9f / 16f;
   private static final float MENU_TABLE_HEIGHT_RATIO = 1f / 2f;
   private static final float MENU_TABLE_RIGHT_OFFSET = 1f / 8f;
+  private static final float WIDTH_MAX_FOR_SMALL_FONT = 500;
+  private static final float WIDTH_MAX_FOR_MEDIUM_FONT = 750;
+  private static final float PADDING_FOR_SMALL_FONT = 0;
+  private static final float PADDING_FOR_MEDIUM_FONT = 15;
+  private static final float PADDING_FOR_LARGE_FONT = 25;
 
   @Override
   public void create() {
     super.create();
     addActors();
   }
-
+  
   private void addActors() {
     table = new Table();
 
@@ -39,12 +46,17 @@ public class MainMenuDisplay extends UIComponent {
             ServiceLocator.getResourceService()
                 .getAsset("images/background-menu-screen.png", Texture.class));
 
-    resize((int) stage.getWidth(), (int) stage.getHeight());
+    TextButton startBtn = new TextButton("Start", skin, "menu-button-large");
+    TextButton loadBtn = new TextButton("Load", skin, "menu-button-large");
+    TextButton settingsBtn = new TextButton("Settings", skin, "menu-button-large");
+    TextButton exitBtn = new TextButton("Exit", skin, "menu-button-large");
 
-    TextButton startBtn = new TextButton("Start", skin, "menu-button");
-    TextButton loadBtn = new TextButton("Load", skin, "menu-button");
-    TextButton settingsBtn = new TextButton("Settings", skin, "menu-button");
-    TextButton exitBtn = new TextButton("Exit", skin, "menu-button");
+    menuButtons = new ArrayList<TextButton>();
+    menuButtons.add(startBtn);
+    menuButtons.add(loadBtn);
+    menuButtons.add(settingsBtn);
+    menuButtons.add(exitBtn);
+
 
     // Triggers an event when the button is pressed
     startBtn.addListener(
@@ -85,12 +97,14 @@ public class MainMenuDisplay extends UIComponent {
         });
 
     table.align(Align.center);
-    table.add(startBtn).padRight(50f);
+    table.add(startBtn);
 
     // load button is removed until the save/load functionality has been added
     //table.add(loadBtn).padRight(50f);
-    table.add(settingsBtn).padRight(50f);
+    table.add(settingsBtn);
     table.add(exitBtn);
+
+    resize((int) stage.getWidth(), (int) stage.getHeight());
 
     stage.addActor(background);
     stage.addActor(table);
@@ -102,17 +116,62 @@ public class MainMenuDisplay extends UIComponent {
   }
 
   public void resize(int width, int height) {
+
       float backgroundWidth = width;
       float backgroundHeight = width * BACKGROUND_IMAGE_ASPECT;
+      float backgroundX = width - backgroundWidth;
+      float backgroundY = height - backgroundHeight;
+      boolean restrictedByHeight = (backgroundHeight > height) ? true : false;
+
+      if (restrictedByHeight) {
+          backgroundWidth = height * (1f / BACKGROUND_IMAGE_ASPECT);
+          backgroundHeight = height;
+          backgroundX = width/2 - backgroundWidth/2;
+          backgroundY = height/2 - backgroundHeight/2;
+      }
+
       background.setWidth(backgroundWidth);
       background.setHeight(backgroundHeight);
-      background.setPosition(width - backgroundWidth, height - backgroundHeight);
+      background.setPosition(backgroundX, backgroundY);
 
       float tableWidth = backgroundWidth - (backgroundWidth * MENU_TABLE_RIGHT_OFFSET);
       float tableHeight = backgroundHeight * MENU_TABLE_HEIGHT_RATIO;
+      float tableX = width - backgroundWidth;
+      float tableY = height - backgroundHeight;
+
+      if (restrictedByHeight) {
+          tableX = backgroundX;
+          tableY = backgroundY;
+      }
+
       table.setWidth(tableWidth);
       table.setHeight(tableHeight);
-      table.setPosition(width - backgroundWidth, height - backgroundHeight);
+      table.setPosition(tableX, tableY);
+
+      adjustMenuButtonsOnResize();
+  }
+
+  private void adjustMenuButtonsOnResize() {
+      float tableWidth = table.getWidth();
+
+      if (tableWidth < WIDTH_MAX_FOR_SMALL_FONT) {
+          changeMenuButtonStyles("menu-button-small", PADDING_FOR_SMALL_FONT);
+      } else if (tableWidth < WIDTH_MAX_FOR_MEDIUM_FONT) {
+          changeMenuButtonStyles("menu-button-medium", PADDING_FOR_MEDIUM_FONT);
+      } else {
+          changeMenuButtonStyles("menu-button-large", PADDING_FOR_LARGE_FONT);
+      }
+  }
+
+  private void changeMenuButtonStyles(String style, float padding) {
+      for (TextButton menuButton : menuButtons) {
+          Button.ButtonStyle newButtonStyle = skin.get( style, TextButton.TextButtonStyle.class );
+          menuButton.setStyle(newButtonStyle);
+          Array<Cell> cells = table.getCells();
+          for (Cell cell : cells) {
+              cell.pad(padding);
+          }
+      }
   }
 
   @Override

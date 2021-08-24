79ddcd310c39647f114df07c0d49084640d98bdb
implex001 <joseph.goh@uqconnect.edu.au>
2021-08-24 16:06:37 +1000

    Added dialogue input and styled TextDialogueBox. Finished implementing base Dialogue

--- source/core/src/main/com/deco2800/game/components/dialoguebox/Dialogue.java
+++ source/core/src/main/com/deco2800/game/components/dialoguebox/Dialogue.java
@@ -3,20 +3,50 @@ package com.deco2800.game.components.dialoguebox;
 
 import com.deco2800.game.components.Component;
 
+import java.util.List;
+
+/**
+ * Dialogue container storing text
+ * <p>Has similar functionality to an Iterator. This has been created in case that
+ * advanced functionality is needed later</p>
+ */
 public class Dialogue extends Component {
-    private String[] allDialogue;
+    private final List<String> allDialogue;
     private int dialogueIndex;
 
-    public Dialogue(String[] allDialogue) {
+    /**
+     * Creates a new Dialogue
+     * @param allDialogue List containing text to be shown
+     */
+    public Dialogue(List<String> allDialogue) {
         this.allDialogue = allDialogue;
         dialogueIndex = 0;
     }
 
-    public void next() {
-        dialogueIndex++;
+    /**
+     * Advances dialogue by one and returns it
+     * @return Next text in dialogue. Repeats last text if at end.
+     */
+    public String next() {
+        if (hasNext()) {
+            dialogueIndex++;
+        }
+        return getCurrentDialogue();
     }
 
+    /**
+     * Returns whether there is dialogue left.
+     * @return True if there is dialogue left. False if else.
+     */
+    public boolean hasNext() {
+        return dialogueIndex < allDialogue.size() - 1;
+    }
+
+    /**
+     * Gets the current dialogue text
+     * @return String containing dialogue
+     */
     public String getCurrentDialogue() {
-        return allDialogue[dialogueIndex];
+        return allDialogue.get(dialogueIndex);
     }
 }
--- source/core/src/main/com/deco2800/game/components/dialoguebox/TextDialogueBox.java
+++ source/core/src/main/com/deco2800/game/components/dialoguebox/TextDialogueBox.java
@@ -1,38 +1,82 @@
 package com.deco2800.game.components.dialoguebox;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.deco2800.game.ui.UIComponent;
 
+/**
+ * Text display of dialogue
+ */
 public class TextDialogueBox extends UIComponent {
     private Dialogue dialogue;
     private Table rootTable;
     private Label displayText;
+    private static final float WIDTH = 500f;
+    private static final float HEIGHT = 100f;
 
+    /**
+     * Creates a new TextDialogueBox
+     * @param dialogue Dialogue to be displayed
+     */
     public TextDialogueBox(Dialogue dialogue){
         super();
         this.dialogue = dialogue;
     }
 
+    /**
+     * Creates UI and event listeners
+     */
     @Override
     public void create() {
         super.create();
         addActors();
+        entity.getEvents().addListener("advanceDialogue", this::advanceDialogue);
     }
 
+    /**
+     * Creates UI actors and styles them
+     */
     private void addActors() {
         rootTable = new Table();
-        displayText = new Label(dialogue.getCurrentDialogue(), skin, "font");
+        displayText = new Label(dialogue.getCurrentDialogue(), skin);
+        displayText.setWrap(true);
+        rootTable.add(displayText).width(WIDTH);
+
+        rootTable.setSkin(skin);
+        rootTable.setBackground("list-c");
+        rootTable.pack();
+
+        stage.addActor(rootTable);
     }
 
+    /**
+     * Dialogue object being displayed
+     * @return Dialogue
+     */
     public Dialogue getDialogue() {
         return dialogue;
     }
 
+    /**
+     * Advances dialogue and updates text display
+     */
+    public void advanceDialogue(){
+        displayText.setText(dialogue.next());
+    }
+
     @Override
     protected void draw(SpriteBatch batch) {
-
+        //Update positioning
+        int screenHeight = Gdx.graphics.getHeight();
+        int screenWidth = Gdx.graphics.getWidth();
+        rootTable.setWidth(WIDTH);
+        rootTable.setHeight(HEIGHT);
+        rootTable.setPosition(screenWidth / 2f - rootTable.getWidth() / 2, screenHeight / 15f);
     }
 
     @Override

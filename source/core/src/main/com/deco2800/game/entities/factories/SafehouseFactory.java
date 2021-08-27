e1b1f1452459881cc1891d9f6f58bb65bb3ab3b9
ICBIX <creepercast888@gmail.com>
2021-08-27 10:16:39 +1000

    Moved Safehouse generation to SafehouseFactory class.

---
+++ source/core/src/main/com/deco2800/game/entities/factories/SafehouseFactory.java
@@ -0,0 +1,23 @@
+package com.deco2800.game.entities.factories;
+
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.physics.PhysicsLayer;
+import com.deco2800.game.physics.PhysicsUtils;
+import com.deco2800.game.physics.components.ColliderComponent;
+import com.deco2800.game.physics.components.PhysicsComponent;
+import com.deco2800.game.rendering.TextureRenderComponent;
+
+public class SafehouseFactory {
+    public static Entity createSafehouse() {
+        Entity safehouse = new Entity()
+                .addComponent(new TextureRenderComponent("images/safehouse.png"))
+                .addComponent(new PhysicsComponent().setBodyType(BodyType.StaticBody))
+                .addComponent(new ColliderComponent().setLayer(PhysicsLayer.OBSTACLE));
+        safehouse.getComponent(PhysicsComponent.class).setBodyType(BodyType.StaticBody);
+        safehouse.getComponent(TextureRenderComponent.class).scaleEntity();
+        safehouse.scaleHeight(2.5f);
+        PhysicsUtils.setScaledCollider(safehouse, 0.5f, 0.2f);
+        return safehouse;
+    }
+}

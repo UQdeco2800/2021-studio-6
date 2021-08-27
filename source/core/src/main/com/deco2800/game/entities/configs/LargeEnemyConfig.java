819c494ebb725767349d96af663992d0c70024d5
Niklas <nikmatha@gmail.com>
2021-08-27 11:19:32 +1000

    Minor changes

--- source/core/src/main/com/deco2800/game/entities/configs/LargeEnemyConfig.java
+++ source/core/src/main/com/deco2800/game/entities/configs/LargeEnemyConfig.java
@@ -1,5 +1,8 @@
 package com.deco2800.game.entities.configs;
 
+/**
+ * Defines the properties stored in large enemy config files to be loaded by the NPC Factory.
+ */
 public class LargeEnemyConfig extends BaseEntityConfig {
     public int speed_x;
     public int speed_y;

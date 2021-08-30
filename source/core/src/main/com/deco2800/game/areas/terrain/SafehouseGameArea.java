3f31fc5136de0de35014d5bba83760783a47091a
BungyStudios <bungystudios@gmail.com>
2021-08-30 11:28:43 +1000

    Fixed TouchTeleportComponent and SafeHouseFactory

---
+++ source/core/src/main/com/deco2800/game/areas/terrain/SafehouseGameArea.java
@@ -0,0 +1,87 @@
+package com.deco2800.game.areas;
+
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.math.GridPoint2;
+import com.badlogic.gdx.math.Vector2;
+import com.deco2800.game.areas.terrain.TerrainFactory;
+import com.deco2800.game.areas.terrain.TerrainFactory.TerrainType;
+import com.deco2800.game.entities.Entity;
+import com.deco2800.game.entities.factories.NPCFactory;
+import com.deco2800.game.entities.factories.ObstacleFactory;
+import com.deco2800.game.entities.factories.PlayerFactory;
+import com.deco2800.game.entities.factories.SafehouseFactory;
+import com.deco2800.game.utils.math.GridPoint2Utils;
+import com.deco2800.game.utils.math.RandomUtils;
+import com.deco2800.game.services.ResourceService;
+import com.deco2800.game.services.ServiceLocator;
+import com.deco2800.game.components.gamearea.GameAreaDisplay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Forest area for the demo game with trees, a player, and some enemies. */
+public class SafehouseGameArea extends GameArea {
+    private static final Logger logger = LoggerFactory.getLogger(ForestGameArea.class);
+    private static final GridPoint2 PLAYER_SPAWN = new GridPoint2(10, 10);
+    private static final String[] safehouseTextures = {
+            "images/safehouse/"
+    };
+
+    private final TerrainFactory terrainFactory;
+
+    private Entity player;
+
+    public SafehouseGameArea(TerrainFactory terrainFactory) {
+        super();
+        this.terrainFactory = terrainFactory;
+    }
+
+    /** Create the game area, including terrain, static entities (trees), dynamic entities (player) */
+    @Override
+    public void create() {
+        loadAssets();
+        displayUI();
+        player = spawnPlayer();
+    }
+
+    private void displayUI() {
+        Entity ui = new Entity();
+        ui.addComponent(new GameAreaDisplay("Safehouse"));
+        spawnEntity(ui);
+    }
+
+    private void spawnTerrain() {
+        // Background terrain
+        terrain = terrainFactory.createTerrain(TerrainType.FOREST_DEMO);
+        spawnEntity(new Entity().addComponent(terrain));
+    }
+
+
+    private Entity spawnPlayer() {
+        Entity newPlayer = PlayerFactory.createPlayer();
+        spawnEntityAt(newPlayer, PLAYER_SPAWN, true, true);
+        return newPlayer;
+    }
+
+    private void loadAssets() {
+        logger.debug("Loading assets");
+        ResourceService resourceService = ServiceLocator.getResourceService();
+        resourceService.loadTextures(safehouseTextures);
+
+        while (!resourceService.loadForMillis(10)) {
+            // This could be upgraded to a loading screen
+            logger.info("Loading... {}%", resourceService.getProgress());
+        }
+    }
+
+    private void unloadAssets() {
+        logger.debug("Unloading assets");
+        ResourceService resourceService = ServiceLocator.getResourceService();
+        resourceService.unloadAssets(safehouseTextures);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        this.unloadAssets();
+    }
+}
---
+++ source/core/src/main/com/deco2800/game/components/TouchTeleportComponent.java
@@ -0,0 +1,47 @@
+package com.deco2800.game.components;
+
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.deco2800.game.areas.ForestGameArea;
+import com.deco2800.game.areas.SafehouseGameArea;
+import com.deco2800.game.areas.terrain.TerrainFactory;
+import com.deco2800.game.physics.PhysicsLayer;
+import com.deco2800.game.physics.components.HitboxComponent;
+import com.deco2800.game.screens.MainGameScreen;
+
+/**
+ * When this entity touches a valid object's hitbox, teleport them to another level or area.
+ *
+ * <p>Requires, HitboxComponent on this entity.
+ */
+public class TouchTeleportComponent extends Component {
+    private short targetLayer;
+    private HitboxComponent hitboxComponent;
+
+    /**
+     * Create a component which teleports entities on collision.
+     * @param targetLayer The physics later of the target's collider.
+     */
+    public TouchTeleportComponent(short targetLayer) {
+        this.targetLayer = targetLayer;
+    }
+
+    public void create() {
+        entity.getEvents().addListener("collisionStart", this::onCollisionStart);
+        hitboxComponent = entity.getComponent(HitboxComponent.class);
+    }
+
+    private void onCollisionStart(Fixture me, Fixture other) {
+        if (hitboxComponent.getFixture() != me) {
+            // Not triggered by hitbox, ignore
+            return;
+        }
+
+        if (!PhysicsLayer.contains(targetLayer, other.getFilterData().categoryBits)) {
+            // Doesn't match our target layer, ignore
+            return;
+        }
+
+        // Try to teleport entity
+        System.out.println("Touched Safehouse");
+    }
+}

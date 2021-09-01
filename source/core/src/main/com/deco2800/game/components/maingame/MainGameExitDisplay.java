1dd7d5f0ddef4e1068f500e3119f5bcbbd56f207
kainphoton <luckyphotonkain@protonmail.com>
2021-09-01 13:31:01 +1000

    Update pause display, now is linked with ESC key
    The pause displays when you pause the game via ESCAPE key
    it displays when you pause the game, and goes away when
    you resume the game.  The continue button doesn't work
    for now and also the pause display is not resizable
    at the moment as well.  At least the exit button on the
    pause display works.  The color and design for the pause
    screen is new skin (Gdx.files.internal(flat-earth/skin/flat-earth-ui.json))

--- source/core/src/main/com/deco2800/game/components/maingame/MainGameExitDisplay.java
+++ source/core/src/main/com/deco2800/game/components/maingame/MainGameExitDisplay.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.deco2800.game.services.ServiceLocator;
 import com.deco2800.game.ui.UIComponent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +20,7 @@ public class MainGameExitDisplay extends UIComponent {
   private static final Logger logger = LoggerFactory.getLogger(MainGameExitDisplay.class);
   private static final float Z_INDEX = 2f;
   private Table table;
+  private Window pauseWindow;
 
   @Override
   public void create() {
@@ -26,6 +28,26 @@ public class MainGameExitDisplay extends UIComponent {
     addActors();
   }
 
+  /**
+   * Checks to see if the game is paused by ESC key, if it is display the pause window
+   * Game resumes by pressing ESC key again and the pause window goes away
+   */
+  @Override
+  public void update() {
+    super.update();
+
+    // If game is paused then turn off screen
+    if (ServiceLocator.getTimeSource().getDeltaTime() == 0f) {
+      pauseWindow.setVisible(true);
+    } else {
+      pauseWindow.setVisible(false);
+    }
+  }
+
+  /**
+   * Adds the exit button and pause display, ideally the pause display should be
+   * in its own MainGamePauseDisplay component tbh, maybe update code and javadoc for future sprints
+   */
   private void addActors() {
     table = new Table();
     table.top().right();
@@ -44,18 +66,29 @@ public class MainGameExitDisplay extends UIComponent {
       });
 
     // =======================  Window Pause test  =============================
-    Window pause = new Window("PAUSE - Placeholder Screen", skin);
+    // TODO: For UI team, read below in multicomment section
+    /*
+     This section below should really be in its own MainGamePauseDisplay.java component
+     but I had no success with the timeframe I had as it crashes when I click
+     the exit button on pause display, this may be fixed in future sprints by
+     someone smarter than me.
+
+     Also may need to update the skin json file for the display to suit the theme?
+     Pause Window is also not resizable at the moment.
+     Overall this part is really messy.
+     */
+    pauseWindow = new Window("PAUSE - Placeholder Screen", skin);
     TextButton continueButton = new TextButton("Continue", skin);
     TextButton exitButton = new TextButton ("Exit", skin);
 
-    pause.add(continueButton);
-    pause.add(exitButton);
+    pauseWindow.add(continueButton);
+    pauseWindow.add(exitButton);
 
     // Click Events for continue & exit button
     continueButton.addListener(new ClickListener() {
       @Override
       public void clicked(InputEvent event, float x, float y) {
-        pause.setVisible(false);
+        // do nothing for now
       }
     });
     exitButton.addListener(new ClickListener() {
@@ -66,12 +99,12 @@ public class MainGameExitDisplay extends UIComponent {
     });
 
     // Set and position pause window screen
-    pause.setSize(stage.getWidth() / 3f, stage.getHeight() / 2f);
-    pause.setMovable(false);
-    pause.setPosition(stage.getWidth() / 2 - pause.getWidth() / 2,
-            stage.getHeight() / 2 - pause.getHeight() / 2);
+    pauseWindow.setSize(stage.getWidth() / 3f, stage.getHeight() / 2f);
+    pauseWindow.setMovable(false);
+    pauseWindow.setPosition(stage.getWidth() / 2 - pauseWindow.getWidth() / 2,
+            stage.getHeight() / 2 - pauseWindow.getHeight() / 2);
 
-    stage.addActor(pause);
+    stage.addActor(pauseWindow);
     table.add(mainMenuBtn).padTop(10f).padRight(10f);
     stage.addActor(table);
   }

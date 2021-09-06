8d7458a60b84de631a955302fdf4b20c6f32c516
25Ericcheong <ericcheongkaikit@gmail.com>
2021-09-06 22:42:33 +1000

    added ammo variable in inventory component along with relevant getters and setters

--- source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
@@ -60,7 +60,7 @@ public class PlayerFactory {
                     .addComponent(new PlayerActions(stats.woundState))
                     .addComponent(new PlayerCombatStatsComponent(stats.health, stats.baseAttack, stats.woundState,
                             stats.baseRangedAttack, stats.defenceLevel))
-                    .addComponent(new InventoryComponent(stats.gold))
+                    .addComponent(new InventoryComponent(stats.gold, stats.bulletNumber))
                     .addComponent(inputComponent)
                     .addComponent(new PlayerStatsDisplay())
                     .addComponent(new PlayerRangeAttackComponent())

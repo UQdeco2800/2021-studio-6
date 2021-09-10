10bc3fbf9724275dddcb6182a4ff732ba54f1569
25Ericcheong <ericcheongkaikit@gmail.com>
2021-09-10 23:06:47 +1000

    created ENUM for item types

--- source/core/src/main/com/deco2800/game/components/ItemComponent.java
+++ source/core/src/main/com/deco2800/game/components/ItemComponent.java
@@ -1,13 +1,25 @@
 package com.deco2800.game.components;
 
+import com.deco2800.game.items.Items;
+
 /**
  * Used to give items 'behaviors' that are relevant to that specific item
  * this allows player entity to identify the kinds of items that are picked up
  * during the game
  */
 public class ItemComponent extends Component {
+    private Items itemType;
 
-    public ItemComponent() {
+    public ItemComponent(Items itemType) {
+        itemType = itemType;
+    }
 
+    /**
+     * This will return item type to make item entity distinguishable by player
+     * when item is picked up
+     * @return item type designated to item entity created
+     */
+    public Items getItemType() {
+        return itemType;
     }
 }
--- source/core/src/main/com/deco2800/game/entities/factories/ItemFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/ItemFactory.java
@@ -3,6 +3,7 @@ package com.deco2800.game.entities.factories;
 import com.deco2800.game.components.ItemComponent;
 import com.deco2800.game.components.TouchAttackComponent;
 import com.deco2800.game.entities.Entity;
+import com.deco2800.game.items.Items;
 import com.deco2800.game.physics.components.ColliderComponent;
 import com.deco2800.game.physics.components.PhysicsComponent;
 import com.deco2800.game.rendering.TextureRenderComponent;
@@ -17,7 +18,7 @@ public class ItemFactory {
         return new Entity()
                 .addComponent(new PhysicsComponent())
                 .addComponent(new ColliderComponent().setSensor(true))
-                .addComponent(new ItemComponent())
+                .addComponent(new ItemComponent(Items.AMMO))
                 .addComponent(new TextureRenderComponent("images/playeritems/pickupammo.png"));
     }
 }

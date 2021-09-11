204be70e97f24c652c4378e70681352c243389de
Bluehalomango <bluehalomango@hotmail.co.uk>
2021-09-12 09:54:35 +1000

    Finished hurt animations for player and added new isMoving function to PlayerActions for use by other classes.

--- source/core/src/main/com/deco2800/game/components/player/PlayerAnimationController.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerAnimationController.java
@@ -1,7 +1,10 @@
 package com.deco2800.game.components.player;
 
+import com.badlogic.gdx.math.Vector2;
 import com.deco2800.game.components.Component;
 import com.deco2800.game.rendering.AnimationRenderComponent;
+import com.deco2800.game.services.GameTime;
+import com.deco2800.game.services.ServiceLocator;
 
 /**
  * This class listens to events relevant to a player's state and plays the animation when one
@@ -10,6 +13,10 @@ import com.deco2800.game.rendering.AnimationRenderComponent;
  */
 public class PlayerAnimationController extends Component {
   AnimationRenderComponent animator;
+  private final GameTime timeSource = ServiceLocator.getTimeSource();
+  private static final long hurtDuration = 1000;
+  private boolean hurtActive = false;
+  private long hurtTime;
   private int lastDirection;
 
   @Override
@@ -27,31 +34,63 @@ public class PlayerAnimationController extends Component {
     animator.startAnimation("front");
   }
 
+  @Override
+  public void update() {
+    if (timeSource.getTimeSince(hurtTime) >= hurtDuration) {
+      hurtActive = false;
+      if (!entity.getComponent(PlayerActions.class).isMoving()) {
+        animateIdle();
+      }
+    }
+  }
+
+  private boolean checkHurt() {
+    return hurtActive;
+  }
+
   void animateDead() {
     animator.startAnimation("dead-right");
   }
 
   void animateMoveDown() {
-    animator.startAnimation("front-run");
+    if (checkHurt()) {
+      animator.startAnimation("front-run-hurt");
+    } else {
+      animator.startAnimation("front-run");
+    }
     lastDirection = 1;
   }
 
   void animateMoveRight() {
-    animator.startAnimation("right-run");
+    if (checkHurt()) {
+      animator.startAnimation("right-run-hurt");
+    } else {
+      animator.startAnimation("right-run");
+    }
     lastDirection = 2;
   }
 
   void animateMoveUp() {
-    animator.startAnimation("back-run");
+    if (checkHurt()) {
+      animator.startAnimation("back-run-hurt");
+    } else {
+      animator.startAnimation("back-run");
+    }
     lastDirection = 3;
   }
 
   void animateMoveLeft() {
-    animator.startAnimation("left-run");
+    if (checkHurt()) {
+      animator.startAnimation("left-run-hurt");
+    } else {
+      animator.startAnimation("left-run");
+    }
     lastDirection = 4;
   }
 
   void animateHurt() {
+    hurtActive = true;
+    hurtTime = timeSource.getTime() + hurtDuration;
     switch (lastDirection) {
       case 1:
         animator.startAnimation("front-hurt");
@@ -68,19 +107,36 @@ public class PlayerAnimationController extends Component {
     }
   }
   void animateIdle() {
-    switch (lastDirection) {
-      case 1:
-        animator.startAnimation("front");
-        break;
-      case 2:
-        animator.startAnimation("right");
-        break;
-      case 3:
-        animator.startAnimation("back");
-        break;
-      case 4:
-        animator.startAnimation("left");
-        break;
+    if(checkHurt()) {
+      switch (lastDirection) {
+        case 1:
+          animator.startAnimation("front-hurt");
+          break;
+        case 2:
+          animator.startAnimation("right-hurt");
+          break;
+        case 3:
+          animator.startAnimation("back-hurt");
+          break;
+        case 4:
+          animator.startAnimation("left-hurt");
+          break;
+      }
+    } else {
+      switch (lastDirection) {
+        case 1:
+          animator.startAnimation("front");
+          break;
+        case 2:
+          animator.startAnimation("right");
+          break;
+        case 3:
+          animator.startAnimation("back");
+          break;
+        case 4:
+          animator.startAnimation("left");
+          break;
+      }
     }
   }
 }

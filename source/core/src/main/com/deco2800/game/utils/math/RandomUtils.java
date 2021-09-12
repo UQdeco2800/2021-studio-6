d3f6c82c9876126c88a47cb5d66eda8b4c43fafc
25Ericcheong <ericcheongkaikit@gmail.com>
2021-09-13 08:28:58 +1000

    fix bug and security hotspot

--- source/core/src/main/com/deco2800/game/utils/math/RandomUtils.java
+++ source/core/src/main/com/deco2800/game/utils/math/RandomUtils.java
@@ -3,7 +3,6 @@ package com.deco2800.game.utils.math;
 import com.badlogic.gdx.math.GridPoint2;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
-import java.util.Random;
 
 public class RandomUtils {
   public static Vector2 random(Vector2 start, Vector2 end) {
@@ -15,8 +14,7 @@ public class RandomUtils {
   }
 
   public static int randomInt(int maxRandomInt) {
-    Random random = new Random();
-    return random.nextInt(maxRandomInt);
+    return MathUtils.random(maxRandomInt);
   }
 
   private RandomUtils() {

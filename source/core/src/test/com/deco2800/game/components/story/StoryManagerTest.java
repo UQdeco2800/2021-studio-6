24b453121f483255f48d9102a78d6f288237386e
implex001 <joseph.goh@uqconnect.edu.au>
2021-09-16 14:04:26 +1000

    Added tests to StoryManager

--- source/core/src/test/com/deco2800/game/components/story/StoryManagerTest.java
+++ source/core/src/test/com/deco2800/game/components/story/StoryManagerTest.java
@@ -1,8 +1,10 @@
 package com.deco2800.game.components.story;
 
 import com.deco2800.game.extensions.GameExtension;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,6 +14,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class StoryManagerTest {
 
     StoryManager storyManager = StoryManager.getInstance();
+
+    @Test
+    void returnNullOnNoStoryLoaded() {
+        assertNull(storyManager.displayStory());
+    }
 
     @Test
     void allStoryNamesUsed() {

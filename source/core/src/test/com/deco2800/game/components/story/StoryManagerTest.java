2212dbbb5be87de54376ee72219b6b0f20533e92
implex001 <joseph.goh@uqconnect.edu.au>
2021-09-16 13:35:34 +1000

    Added tests to StoryManager

---
+++ source/core/src/test/com/deco2800/game/components/story/StoryManagerTest.java
@@ -0,0 +1,23 @@
+package com.deco2800.game.components.story;
+
+import com.deco2800.game.extensions.GameExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
+class StoryManagerTest {
+
+    StoryManager storyManager = StoryManager.getInstance();
+
+    @Test
+    void allStoryNamesUsed() {
+        StoryNames[] storyNames = StoryNames.values();
+
+        //The plus 1 is to account for the NONE story.
+        assertEquals(storyNames.length, storyManager.getScenesConfigs().size() + 1);
+    }
+}

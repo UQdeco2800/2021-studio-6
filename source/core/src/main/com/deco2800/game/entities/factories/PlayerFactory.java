cb17279ee716014fa99ca777e6a2fdc444178225
25Ericcheong <ericcheongkaikit@gmail.com>
2021-09-27 15:20:41 +1000

    fixed updating player's melee weapon type

--- source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
+++ source/core/src/main/com/deco2800/game/entities/factories/PlayerFactory.java
@@ -117,6 +117,7 @@ public class PlayerFactory {
     PhysicsUtils.setScaledCollider(player, 0.6f, 0.3f);
     player.getComponent(ColliderComponent.class).setDensity(1.5f);
     player.getComponent(PlayerMeleeAttackComponent.class).setMeleeWeaponType(Items.getMeleeWeapon(meleeWeaponType));
+    player.getComponent(PlayerMeleeAttackComponent.class).setWeapon(meleeFilePath);
     return player;
   }
 

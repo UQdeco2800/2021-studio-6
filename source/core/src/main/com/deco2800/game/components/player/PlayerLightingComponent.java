57a4d204f60f4262c0d5947b5c9e42da7400ef66
SBHATTA19 <sambit.bhattacharya@live.com.au>
2021-09-29 04:15:36 +1000

    Added basic file to toggle torch state (not implemented).

--- source/core/src/main/com/deco2800/game/components/player/PlayerLightingComponent.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerLightingComponent.java
@@ -7,19 +7,27 @@ import com.deco2800.game.lighting.PointLightComponent;
 
 public class PlayerLightingComponent extends PointLightComponent {
 
+    private boolean torchState;
+
     public PlayerLightingComponent (Color color, float distance, float offsetx, float offsety) {
         super(color, distance,offsetx,offsety);
-
+        torchState = false;
     }
 
     @Override
     public void create() {
         super.create();
 
-        entity.getEvents().addListener("toggleTorch", this::TorchState);
+        entity.getEvents().addListener("toggleTorch", this::ToggleTorchState);
     }
 
-    private void TorchState() {
+    private void ToggleTorchState() {
+        if (torchState) {
+            this.torchState = false;
+        }
+        else {
+            this.torchState = true;
+        }
 
     }
 }

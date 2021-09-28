46728a9a441f658723d5a9d1bad2692598f1df4f
Chris Kang <kangthchris@gmail.com>
2021-09-29 08:46:09 +1000

    This reverts commit 2c3568a4e82c4030ca2cc6e5158c278eed03327a.

--- source/core/src/main/com/deco2800/game/components/story/stories/TutorialDiologue.java
+++ source/core/src/main/com/deco2800/game/components/story/stories/TutorialDiologue.java
@@ -4,14 +4,13 @@ import com.deco2800.game.components.story.basicdialogue.BasicDialogueConfig;
 
 public class TutorialDiologue extends BasicDialogueConfig {
     protected static final String[] QUOTE= {"Greetings, young man.",
-            "I heard you wanted to become a firefly.",
-            "So I came to give you some advice.",
+            "So you want to become a firefly?",
             "Use W A S D key to move, up, left, down, right.",
             "Press Shift key to dash a short distance. The blue bar indicates cooldown for your dash.",
             "Press Space to attack enemies with your melee weapon.",
             "Press Enter to throw rocks. If you run out of rocks, you can press R key to reload.",
-            "I wish you best of luck in your journey."};
-    protected static final String[] PORTRAIT = {"oldman_npc"};
+            "Good luck have fun!"};
+    protected static final String[] PORTRAIT = {"npc-tut1-portrait"};
     private static final int LENGTH = 7;
 
     public TutorialDiologue(){

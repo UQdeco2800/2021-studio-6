d8488e9146ae2d014b57ff9a948dd115516db26d
implex001 <joseph.goh@uqconnect.edu.au>
2021-10-01 10:34:55 +1000

    Added javadoc

--- source/core/src/main/com/deco2800/game/components/npc/FriendlyNPCTriggerComponent.java
+++ source/core/src/main/com/deco2800/game/components/npc/FriendlyNPCTriggerComponent.java
@@ -10,6 +10,9 @@ import com.deco2800.game.physics.components.PhysicsMovementComponent;
 import com.deco2800.game.services.ServiceLocator;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Input component that triggers a story when SPACE is pressed and is in range of the entity
+ */
 public class FriendlyNPCTriggerComponent extends InputComponent {
     private static org.slf4j.Logger logger = LoggerFactory.getLogger(FriendlyNPCTriggerComponent.class);
     private static final int STATIONARY = 0;
@@ -23,6 +26,9 @@ public class FriendlyNPCTriggerComponent extends InputComponent {
         logger.info("Friendly NPC {} created", name);
     }
 
+    /**
+     * Sets up component and collision listeners
+     */
     @Override
     public void create() {
         super.create();
@@ -31,6 +37,11 @@ public class FriendlyNPCTriggerComponent extends InputComponent {
     }
 
 
+    /**
+     * Called when a key is pressed
+     * @param keycode key
+     * @return if the input was processed in this instance
+     */
     @Override
     public boolean keyDown(int keycode) {
         if (isInNPCRange && keycode == INTERACT_KEY) {
@@ -47,12 +58,22 @@ public class FriendlyNPCTriggerComponent extends InputComponent {
     }
 
 
+    /**
+     * Sets isInNPCRange to true if player is in range
+     * @param me the entity
+     * @param other the other entity being collided with me
+     */
     private void collisionStart(Fixture me, Fixture other) {
         if (PhysicsLayer.contains(PhysicsLayer.PLAYER, other.getFilterData().categoryBits)) {
             isInNPCRange = true;
         }
     }
 
+    /**
+     * Sets isInNPCRange to false if player is in range
+     * @param me the entity
+     * @param other the other entity being collided with me
+     */
     private void collisionEnd(Fixture me, Fixture other){
         if (other.getFilterData().categoryBits == PhysicsLayer.PLAYER) {
             isInNPCRange = false;

f71cc00faba78eb42c6afbf52d88c5f00a0ef7de
Brian Stewart <brianstewarteditor@gmail.com>
2021-10-02 12:58:58 +1000

    turn debug back on for merge to main

--- source/core/src/main/com/deco2800/game/rendering/Renderer.java
+++ source/core/src/main/com/deco2800/game/rendering/Renderer.java
@@ -34,7 +34,7 @@ public class Renderer implements Disposable {
   public Renderer(CameraComponent camera) {
     SpriteBatch spriteBatch = new SpriteBatch();
     DebugRenderer debugRenderer = new DebugRenderer();
-    debugRenderer.setActive(false);
+    debugRenderer.setActive(true);
 
 
     init(

ae01cd96dc4c9cecdeddf42cc5715560fd2d905a
Bluehalomango <bluehalomango@hotmail.co.uk>
2021-10-07 09:06:45 +1000

    Fixed cooldown bar length.

--- source/core/src/main/com/deco2800/game/components/player/PlayerInterfaceDisplay.java
+++ source/core/src/main/com/deco2800/game/components/player/PlayerInterfaceDisplay.java
@@ -66,7 +66,7 @@ public class PlayerInterfaceDisplay extends UIComponent {
         new IndependentAnimator(
             ServiceLocator.getResourceService()
                 .getAsset("images/hud/dashbar.atlas", TextureAtlas.class), false);
-    dashAnimator.addAnimation("dashbar", 1.2f, Animation.PlayMode.NORMAL);
+    dashAnimator.addAnimation("dashbar", 0.4f, Animation.PlayMode.NORMAL);
     dashAnimator.addAnimation("dashbarFull", 0.1f, Animation.PlayMode.NORMAL);
 
     dashAnimator.setCamera(true);

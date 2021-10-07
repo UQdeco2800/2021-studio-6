8ec4bbf8ccd616a7fbec59eebee7887032b5f2d1
Bluehalomango <bluehalomango@hotmail.co.uk>
2021-10-07 11:51:11 +1000

    Added testing for the torch running out.

--- source/core/src/test/com/deco2800/game/components/player/InventoryComponentTest.java
+++ source/core/src/test/com/deco2800/game/components/player/InventoryComponentTest.java
@@ -1,5 +1,6 @@
 package com.deco2800.game.components.player;
 
+import com.badlogic.gdx.math.Vector2;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.extensions.GameExtension;
 import com.deco2800.game.services.GameTime;
@@ -8,12 +9,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(GameExtension.class)
+@ExtendWith(MockitoExtension.class)
 class InventoryComponentTest {
   @Mock GameTime time;
   @BeforeEach
@@ -130,5 +134,147 @@ class InventoryComponentTest {
     inventory.addBandages(100);
     inventory.addBandages(-20);
     assertEquals(80, inventory.getBandages());
+  }
+
+
+  @Test
+  void shouldSetGetTorch() {
+    Entity player = new Entity()
+        .addComponent(new InventoryComponent(150, 5, 10, 1))
+        .addComponent(new PlayerInterfaceDisplay());
+    InventoryComponent inventory = player.getComponent(InventoryComponent.class);
+    assertEquals(1, inventory.getTorch());
+
+    inventory.addTorch(150);
+    assertEquals(151, inventory.getTorch());
+    inventory.addTorch(-50);
+    assertEquals(101, inventory.getTorch());
+  }
+
+  @Test
+  void shouldAddTorch() {
+    Entity player = new Entity()
+        .addComponent(new InventoryComponent(150, 5, 10, 100))
+        .addComponent(new PlayerInterfaceDisplay());
+    InventoryComponent inventory = player.getComponent(InventoryComponent.class);
+    inventory.addTorch(500);
+    assertEquals(600, inventory.getTorch());
+    inventory.addTorch(100);
+    inventory.addTorch(-20);
+    assertEquals(680, inventory.getTorch());
+  }
+
+  @Test
+  void shouldTorchDecrease() {
+    when(time.getTime()).thenReturn(0L);
+    Entity player = new Entity()
+        .addComponent(new InventoryComponent(150, 5, 10, 100))
+        .addComponent(new PlayerInterfaceDisplay());
+
+    InventoryComponent inventory = player.getComponent(InventoryComponent.class);
+    inventory.addTorch(500);
+    assertEquals(600, inventory.getTorch());
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    assertEquals(599, inventory.getTorch());
+  }
+
+  @Test
+  void shouldTorchDecreaseMultiple() {
+    when(time.getTime()).thenReturn(0L);
+    Entity player = new Entity()
+        .addComponent(new InventoryComponent(150, 5, 10, 100))
+        .addComponent(new PlayerInterfaceDisplay());
+
+    InventoryComponent inventory = player.getComponent(InventoryComponent.class);
+
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    assertEquals(97, inventory.getTorch());
+  }
+
+  @Test
+  void shouldTorchNotDecrease() {
+    Entity player = new Entity()
+        .addComponent(new InventoryComponent(150, 5, 10, 100))
+        .addComponent(new PlayerInterfaceDisplay());
+
+    InventoryComponent inventory = player.getComponent(InventoryComponent.class);
+
+    when(time.getTimeSince(0)).thenReturn(100L); // on delay time
+    inventory.update();
+    assertEquals(100, inventory.getTorch());
+  }
+
+  @Test
+  void shouldTorchRunOut() {
+    Entity player = new Entity()
+        .addComponent(new InventoryComponent(150, 5, 10, 3))
+        .addComponent(new PlayerInterfaceDisplay());
+
+    InventoryComponent inventory = player.getComponent(InventoryComponent.class);
+
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+
+    assertEquals(0, inventory.getTorch());
+  }
+
+  @Test
+  void shouldTorchRunOutThenAdd() {
+    Entity player = new Entity()
+        .addComponent(new InventoryComponent(150, 5, 10, 3))
+        .addComponent(new PlayerInterfaceDisplay());
+
+    InventoryComponent inventory = player.getComponent(InventoryComponent.class);
+
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+
+    assertEquals(0, inventory.getTorch());
+    inventory.addTorch(100);
+    assertEquals(100, inventory.getTorch());
+
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    assertEquals(97, inventory.getTorch());
+  }
+
+  @Test
+  void shouldTorchNotDecreaseToggled() {
+    Entity player = new Entity()
+        .addComponent(new InventoryComponent(150, 5, 10, 100))
+        .addComponent(new PlayerInterfaceDisplay());
+
+    InventoryComponent inventory = player.getComponent(InventoryComponent.class);
+
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+
+    inventory.torchToggle();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    assertEquals(99, inventory.getTorch());
+
+    inventory.torchToggle();
+    when(time.getTimeSince(0)).thenReturn(1000L); // on delay time
+    inventory.update();
+    assertEquals(98, inventory.getTorch());
   }
 }

ae3a0644173da7c15271281ca49f3267363fd0a2
Brian Stewart <brianstewarteditor@gmail.com>
2021-10-11 21:31:20 +1000

    enemy increase speed in darkness

--- source/core/src/main/com/deco2800/game/components/npc/EnemyInDarknessController.java
+++ source/core/src/main/com/deco2800/game/components/npc/EnemyInDarknessController.java
@@ -1,5 +1,6 @@
 package com.deco2800.game.components.npc;
 
+import com.badlogic.gdx.math.Vector2;
 import com.deco2800.game.components.Component;
 import com.deco2800.game.physics.components.PhysicsMovementComponent;
 import com.deco2800.game.rendering.AnimationRenderComponent;
@@ -11,20 +12,35 @@ import com.deco2800.game.rendering.AnimationRenderComponent;
 public class EnemyInDarknessController extends Component {
   private PhysicsMovementComponent movementComponent;
   private boolean inDarkness = true;
+  private Vector2 defaultSpeed;
+  private Vector2 inDarkSpeed;
 
   @Override
   public void create() {
     super.create();
-    this.movementComponent = this.entity.getComponent(PhysicsMovementComponent.class);
+    movementComponent = this.entity.getComponent(PhysicsMovementComponent.class);
+    defaultSpeed = movementComponent.getSpeed();
+    inDarkSpeed = new Vector2(defaultSpeed.x * 2, defaultSpeed.y * 2);
+    movementComponent.setSpeed(inDarkSpeed);
     entity.getEvents().addListener("inShadow", this::inDarkness);
     entity.getEvents().addListener("inLight", this::inLight);
   }
 
   void inDarkness() {
-    System.out.println("Dark");
+    if (!inDarkness) {
+      System.out.println("Dark");
+      System.out.println(inDarkSpeed);
+      inDarkness = true;
+      movementComponent.setSpeed(inDarkSpeed);
+    }
   }
 
   void inLight() {
-    System.out.println("Light");
+    if (inDarkness) {
+      System.out.println("Light");
+      System.out.println(defaultSpeed);
+      inDarkness = false;
+      movementComponent.setSpeed(defaultSpeed);
+    }
   }
 }

81258fc445cb7a2a2d91f02313123c10a609dd16
Matthew_Holloway <m.holloway@uq.net.au>
2021-10-14 12:56:21 +1000

    added bossState enum to handle state changes

--- source/core/src/main/com/deco2800/game/components/tasks/BossHealthListener.java
+++ source/core/src/main/com/deco2800/game/components/tasks/BossHealthListener.java
@@ -1,10 +1,20 @@
 package com.deco2800.game.components.tasks;
 
 import com.deco2800.game.ai.tasks.MultiAITaskComponent;
+import com.deco2800.game.components.CombatStatsComponent;
 import com.deco2800.game.components.Component;
 import com.deco2800.game.entities.Entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+
+
+enum BossState {
+    PHASE1,
+    PHASE2,
+    PHASEFINAL,
+}
+
 
 /**
  * LaserListener
@@ -13,6 +23,7 @@ import org.slf4j.LoggerFactory;
 public class BossHealthListener extends Component {
     private static final Logger logger = LoggerFactory.getLogger(BossHealthListener.class);
     private Entity boss;
+    private BossState bossState = BossState.PHASE1;
 
     public BossHealthListener(Entity boss) {
         this.boss = boss;
@@ -24,16 +35,30 @@ public class BossHealthListener extends Component {
     public void create() {
         super.create();
         boss.getEvents().addListener("updateHealth", this::state);
+
     }
 
     /**
      * 'fires' a beam from the boss
      */
     void state(int health) {
+        logger.debug("Boss health {}", health);
+        if(bossState == BossState.PHASE1) {
+            if(health < 80) {
+                bossState = BossState.PHASE2;
+                logger.debug("Boss State change from {} to {}", BossState.PHASE1, bossState);
+                boss.getComponent(MultiAITaskComponent.class).removeAllTasks();
+                //change tasks
 
-        logger.info("Boss health {}", health);
+            }
+        } else if(bossState == BossState.PHASE2) {
+            if(health < 70) {
+                bossState = BossState.PHASEFINAL;
+                logger.debug("Boss State change from {} to {}", BossState.PHASE2, bossState);
+            }
+        }
 
-        boss.getComponent(MultiAITaskComponent.class).removeAllTasks();
+
 
     }
 

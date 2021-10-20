d492b0ab0543fb59be8f4ef37ca2f3c3ea378a13
Yijia Zhang <zhangyijia1997@gmail.com>
2021-10-21 02:49:08 +0900

    Implemented dispose

--- source/core/src/main/com/deco2800/game/components/TorchLightingComponent.java
+++ source/core/src/main/com/deco2800/game/components/TorchLightingComponent.java
@@ -1,13 +1,14 @@
 package com.deco2800.game.components;
 
 import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.utils.Disposable;
 import com.deco2800.game.components.player.InventoryComponent;
 import com.deco2800.game.entities.Entity;
 import com.deco2800.game.physics.PhysicsLayer;
 import com.deco2800.game.physics.components.HitboxComponent;
 import com.deco2800.game.services.ServiceLocator;
 
-public class TorchLightingComponent extends Component{
+public class TorchLightingComponent extends Component implements Disposable {
   private static final int ADD_TORCH = 70;
   private HitboxComponent hitboxComponent;
   private short targetLayer;
@@ -21,6 +22,12 @@ public class TorchLightingComponent extends Component{
   public void create() {
     entity.getEvents().addListener("collisionStart", this::onCollisionStart);
     hitboxComponent = entity.getComponent(HitboxComponent.class);
+  }
+
+  @Override
+  public void dispose() {
+    this.hitboxComponent.dispose();
+    this.playerState.dispose();
   }
 
   private void onCollisionStart(Fixture me, Fixture other) {
